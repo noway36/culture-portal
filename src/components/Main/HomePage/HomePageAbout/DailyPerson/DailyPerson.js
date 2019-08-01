ec772477e520bdea4ed142e709275215d33a8286
slava-ff <slavik-28@mail.ru>
2019-08-02 02:21:54 +0300

    feat: add translation to HomePageAbout: Content, DailyPerson

--- src/components/Main/HomePage/HomePageAbout/DailyPerson/DailyPerson.js
+++ src/components/Main/HomePage/HomePageAbout/DailyPerson/DailyPerson.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './daily-person.css';
 import { Link } from 'gatsby';
+import { withTranslation, Trans } from 'react-i18next';
 
-export default function DailyPerson() {
-    return <div className='home-page-about-daily'>
-        <h3>Фотограф дня</h3>
-        <h4>Анемподистов Михаил Владимирович</h4>
-        <h5>16 марта 1964 г. - 23 января 2018 г.</h5>
-        <p>Белорусский художник, дизайнер, поэт, культуролог, искусствовед.</p>
-        <img src="https://i.ibb.co/0GB7fxV/anempodistov.jpg" alt="Foto" />
-        <Link to="/photographers/anempodistov">
-            <button type="button">Перейти</button>
-        </Link>
-    </div>
+class DailyPerson extends React.Component {
+
+    render() {
+        return <div className='home-page-about-daily'>
+            <h3><Trans>Photographer of the day</Trans></h3>
+            <h4><Trans>Anempodistov Mikhail Vladimirovich</Trans></h4>
+            <h5><Trans>March 16, 1964 - January 23, 2018</Trans></h5>
+            <p><Trans>Belarusian artist, designer, poet, culturologist, art critic.</Trans></p>
+            <img src="https://i.ibb.co/0GB7fxV/anempodistov.jpg" alt="Foto" />
+            <Link to="/photographers/anempodistov">
+                <button type="button"><Trans>Go to</Trans></button>
+            </Link>
+        </div>
+    }
+
 }
+
+export default withTranslation()(DailyPerson);
