c2306c3cf0cd124ec634ff9fa27803f3ba04dda8
esoshyki <esoshyki@yandex.by>
2019-08-04 16:36:45 +0300

    feat: add team info window, but not over

--- src/components/Main/HomePage/Team/Team.js
+++ src/components/Main/HomePage/Team/Team.js
@@ -6,9 +6,13 @@ import { withTranslation, Trans } from 'react-i18next';
 class Team extends React.Component {
     constructor(props) {
         super(props);
+        this.handleOver = this.handleOver.bind(this);
+        this.handleLeave = this.handleLeave.bind(this);
         this.state = {
             creators: [],
             title: {},
+            infoShow: false,
+            coordinates: []
         };
     }
 
@@ -20,6 +24,8 @@ class Team extends React.Component {
                 src: 'team_images/esoshyki.png',
                 alt: 'Faryna',
                 gitHub: 'https://github.com/esoshyki',
+                workDone: 'Team component, global styles of site',
+                show: false
             },
             {
                 id: '2',
@@ -27,6 +33,13 @@ class Team extends React.Component {
                 src: 'team_images/dren.png',
                 alt: 'Dren',
                 gitHub: 'https://github.com/noway36',
+                workDone: `gatsby structure + basic layout and rooting
+                implement photographer pages interaction logic
+                implement youtube-overlay
+                add template json files and 3 new authors
+                content translate
+                `,
+                show: false
             },
             {
                 id: '3',
@@ -34,6 +47,8 @@ class Team extends React.Component {
                 src: 'team_images/ivanchikov.png',
                 alt: 'Ivanchikov',
                 gitHub: 'https://github.com/ilyaivanchikov',
+                workDone: 'googleMap components, loader for map, galllery components',
+                show: false
             },
             {
                 id: '4',
@@ -41,6 +56,8 @@ class Team extends React.Component {
                 src: 'team_images/slava.png',
                 alt: 'Fomin',
                 gitHub: 'https://github.com/slava-ff',
+                workDone: 'Implement i18next configuration, add json lexicon , add language toggle function , rite README about i18next in this project ',
+                show: false
             },
             {
                 id: '5',
@@ -48,6 +65,8 @@ class Team extends React.Component {
                 src: 'team_images/tsyk.png',
                 alt: 'Tsykala',
                 gitHub: 'https://github.com/ihartsykala',
+                workDone: 'add HomePageAbout Head, Content, DailyPerson components',
+                show: false
             },
             {
                 id: '6',
@@ -55,6 +74,8 @@ class Team extends React.Component {
                 src: 'team_images/kemal.png',
                 alt: 'Shlembayev',
                 gitHub: 'https://github.com/slider7',
+                workDone: 'Implement TimeLine component, implement SearchPage component, find, edit and prepare info about photographers',
+                show: false
             },
         ];
         const _title = 'TeamHeader';
@@ -64,9 +85,23 @@ class Team extends React.Component {
         });
     }
 
+    handleOver = (event) => {
+        console.log(event.clientX)
+        this.setState({
+            infoShow: true,
+            coordinates: [event.clientX, event.clientY]})
+    }
+
+    handleLeave = (event,el) => {
+        console.log(el)
+        this.setState({infoShow: false})
+    }
+
     render() {
         const { creators } = this.state;
         const { title } = this.state;
+        const { infoShow } = this.state;
+        const { coordinates } = this.state;
         return (
             <>
                 <div className="team_container">
@@ -81,8 +116,13 @@ class Team extends React.Component {
                                     key={el.id}
                                     id={`teamCard${idx}`}
                                 >
-                                    <div className="teamCard_photo_layer1">
-                                        <div className="teamCard_photo_layer2">
+                                    <div onMouseEnter={this.handleOver}
+                                          onMouseLeave={this.handleLeave}
+                                        className="teamCard_photo_layer1"
+                                        id={`teamCard_photo${idx}`}>
+                                        <div className="teamCard_photo_layer2"
+                                            onMouseEnter = {() => {el.show = true; console.log(el.show)}}
+                                            onMouseLeave = {() => {el.show = false; console.log(el.show)}}>
                                             <div
                                                 className="teamCard_photo"
                                                 style={{
@@ -110,6 +150,13 @@ class Team extends React.Component {
                                             <i className="fab fa-github" />
                                         </a>
                                     </div>
+                                    <div className='teamCard_workDone'
+                                        style={{
+                                            display: infoShow && el.show ? 'block' : 'none',
+                                            left: `${coordinates[0]}px`,
+                                            top: `${coordinates[1]}px`
+                                        }}
+                                    ><p>{el.workDone}</p></div>
                                 </div>
                             );
                         })}
